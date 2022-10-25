cbab4756c450e3161701643003bb5d3b418a8288
laurenedb7 <73172295+laurenedb7@users.noreply.github.com>
2022-10-25 14:29:24 +0200

    add review form

--- app/src/main/java/fr/isep/movietracker/MainActivity.java
+++ app/src/main/java/fr/isep/movietracker/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.isep.movietracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -36,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickAddButton(View view) {
-
+        Intent intent = new Intent(this, NewReviewActivity.class);
+        startActivity(intent);
     }
 
 }
