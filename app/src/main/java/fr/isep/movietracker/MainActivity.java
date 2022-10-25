af5518c4fa26f19033ccafd36c53f15d77278c32
laurenedb7 <73172295+laurenedb7@users.noreply.github.com>
2022-10-25 14:07:28 +0200

    add button to add a review

--- app/src/main/java/fr/isep/movietracker/MainActivity.java
+++ app/src/main/java/fr/isep/movietracker/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.isep.movietracker;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -32,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void onClickAddButton(View view) {
+
     }
 
 }
